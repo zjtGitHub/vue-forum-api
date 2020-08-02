89e230cd9a5c54ba40c7966ebba72ac0b0b133ad
zjtGitHub <947140669@qq.com>
2020-08-02 23:40:00 +0800

    帖子列表接口

---
+++ src/model/Post.js
@@ -0,0 +1,48 @@
+import mongoose from '@/config/DBhelper'
+import moment from 'moment'
+
+const Schema = mongoose.Schema
+
+const PostSchema = new Schema({
+  uid: { type: String, ref: 'users' },
+  title: { type: String },
+  content: { type: String },
+  created: { type: Date },
+  catalog: { type: String },
+  fav: { type: String },
+  isEnd: { type: String },
+  reads: { type: Number },
+  answer: { type: Number },
+  status: { type: String },
+  isTop: { type: String },
+  sort: { type: String },
+  tags: { type: Array }
+})
+
+PostSchema.pre('save', function (next) {
+  this.created = moment().format('YYYY-MM-DD HH:mm:ss')
+  next()
+})
+PostSchema.statics = {
+  /**
+   * 获取文章列表数据
+   * @param {Object} options 筛选条件
+   * @param {String} sort 排序方式
+   * @param {Number} page 分页页数
+   * @param {Number} limit 分页条数
+   */
+  getList: function (options, sort, page, limit) {
+    return this.find(options)
+      .sort({ [sort]: -1 })
+      .skip(page * limit)
+      .limit(limit)
+      .populate({
+        path: 'uid',
+        select: 'username'
+      })
+  }
+}
+
+const PostModel = mongoose.model('post', PostSchema)
+
+export default PostModel
