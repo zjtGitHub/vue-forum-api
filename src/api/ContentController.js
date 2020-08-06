9cf9195e8ff47565a9c422ccd9bca3f9a27e4434
zjtGitHub <947140669@qq.com>
2020-08-06 23:42:36 +0800

    ✨ 添加link tip topWeek接口

--- src/api/ContentController.js
+++ src/api/ContentController.js
@@ -1,4 +1,5 @@
 import Post from '../model/Post'
+import Link from '../model/Link'
 
 class ContentController {
   /**
@@ -7,26 +8,6 @@ class ContentController {
    */
   async getPostList (ctx) {
     const body = ctx.query
-    // 测试数据
-    // const post = new Post({
-    //   title: 'title11111',
-    //   content: 'contentsdfsdfsdfsdfsfsdfsdf',
-    //   catalog: '',
-    //   fav: 20,
-    //   isEnd: '0',
-    //   reads: 10,
-    //   answer: 20,
-    //   status: '0',
-    //   isTop: '0',
-    //   sort: 'created',
-    //   tags: [{
-    //     name: '',
-    //     class: ''
-    //   }]
-    // })
-
-    // const tmp = await post.save()
-
     const sort = body.sort ? body.sort : 'created'
     const page = body.page ? parseInt(body.page) : 0
     const limit = body.limit ? parseInt(body.limit) : 20
@@ -48,6 +29,47 @@ class ContentController {
     }
     console.log(options)
     const result = await Post.getList(options, sort, page, limit)
+    ctx.body = {
+      code: 200,
+      data: result,
+      msg: 'success'
+    }
+  }
+
+  /**
+   * 查询友链
+   * @param {*} ctx
+   */
+  async getLinks (ctx) {
+    const result = await Link.find({ type: 'link' })
+    console.log(result)
+    ctx.body = {
+      code: 200,
+      data: result,
+      msg: 'success'
+    }
+  }
+
+  /**
+   * 温馨提醒
+   * @param {*} ctx
+   */
+  async getTips (ctx) {
+    const result = await Link.find({ type: 'tip' })
+    console.log(result)
+    ctx.body = {
+      code: 200,
+      data: result,
+      msg: 'success'
+    }
+  }
+
+  /**
+   * 本周热议
+   * @param {*} ctx
+   */
+  async getTopWeek (ctx) {
+    const result = await Post.getTopWeek()
     ctx.body = {
       code: 200,
       data: result,
