6e8db507adb52572136a1e0214a9395fe720abe9
zhangjintong <947140669@qq.com>
2020-11-11 18:06:06 +0800

    添加签到接口

--- src/api/ContentController.js
+++ src/api/ContentController.js
@@ -8,6 +8,24 @@ class ContentController {
    */
   async getPostList (ctx) {
     const body = ctx.query
+    // 测试数据
+    // const post = new Post({
+    //   title: 'test title',
+    //   content: 'test content',
+    //   catalog: 'ask',
+    //   fav: 20,
+    //   isEnd: '0',
+    //   reads: '0',
+    //   answer: '0',
+    //   status: '0',
+    //   isTop: '0',
+    //   sort: '0',
+    //   tags: [{
+    //     name: '精华',
+    //     class: ''
+    //   }]
+    // })
+    // const tmp = await post.save()
     const sort = body.sort ? body.sort : 'created'
     const page = body.page ? parseInt(body.page) : 0
     const limit = body.limit ? parseInt(body.limit) : 20
--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
-const DB_URL = 'mongodb://localhost/testdb'
+const DB_URL = 'mongodb://root:example@192.168.0.128/admin'
 const REDIS = {
-  host: '127.0.0.1',
-  port: 6379,
+  host: '192.168.0.128',
+  port: 15001,
   password: '123456'
 }
 const JWT_SECRET = 'a&*38QthAKuiRwISGLotgq^3%^$zvA3A6Hfr8MF$jM*HY4*dWcwAW&9NGp7*b53!'
