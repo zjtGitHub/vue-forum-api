6eb633c6682e4f0b8d710bfa71d4672dcee0ecd1
zhangjintong <947140669@qq.com>
2020-12-03 17:53:24 +0800

    添加修改用户信息接口 使用require.context引入route文件

--- src/routes/modules/UserRouter.js
+++ src/routes/modules/UserRouter.js
@@ -1,10 +1,10 @@
 import Router from 'koa-router'
-import UserController from '../api/UserController'
+import UserController from '@/api/UserController'
 
 const router = new Router()
 
 router.prefix('/user')
 
 router.get('/fav', UserController.userSign)
-
+router.post('/basic', UserController.updateUserInfo)
 export default router
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,15 @@
 import combineRoutes from 'koa-combine-routers'
 
-import PublicRouter from './PublicRouter'
-import LoginRouter from './LoginRouter'
-import UserRouter from './UserRouter'
+// 遍历modules下的所有js文件，
+const moduleFiles = require.context('./modules', true, /\.js$/)
+// 使用reduce拼接combineRoutes需要的数据结构
+const modules = moduleFiles.keys().reduce((items, path) => {
+  const value = moduleFiles(path)
+  items.push(value.default)
+  return items
+}, [])
+console.log(modules)
 
 export default combineRoutes(
-  PublicRouter,
-  LoginRouter,
-  UserRouter
+  modules
 )
